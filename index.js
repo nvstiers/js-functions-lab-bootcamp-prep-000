01e0569d7086f335a7216e9cc1eeb1ab7da6bd1e
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-13 06:01:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function halidayCountdown(days, holiday){
+function holidayCountdown(days, holiday){
   return `It's ${days} days until ${holiday}!`
 }
